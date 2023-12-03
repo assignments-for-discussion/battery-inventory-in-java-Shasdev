de6f5c9c3a53680679ec2260cafdc3c341fbe137
M Shashank Dev Padakannaya <99595131+Shasdev@users.noreply.github.com>
2023-12-03 17:34:17 +0530

    Update Main.java

--- bunchbysoh/Main.java
+++ bunchbysoh/Main.java
@@ -11,18 +11,18 @@ public class Main {
     CountsBySoH counts = new CountsBySoH();
     for( int capacity: presentCapacities)
       {
-        double soh= 100.0 * capacity/120.0;
-        if(soh>80.0)
+        double soh= 100.0 * capacity/120.0; //since rated_capacity is 120Ah for all use the same
+        if(soh>80.0)    //if soh lies between 80.0 % and 100.0 % increase the count of healthy(means battery is healthy)
         {
           counts.healthy++;
         }
-        else if(soh>62.0)
+        else if(soh>62.0)   //if soh lies between 62.0 % and 80.0 % increase the count of exchange(means battery needs to be exchanged)
         {
           counts.exchange++;
         }
         else
         {
-          counts.failed++;
+          counts.failed++;  //if soh lies below 62.0 % increase the count of failed(means battery has failed)
         }
       }
     return counts;
@@ -35,6 +35,9 @@ public class Main {
     assert(counts.healthy == 2);
     assert(counts.exchange == 3);
     assert(counts.failed == 1);
+    System.out.println("Count of Healthy batteries are:"+counts.healthy+"\n");
+    System.out.println("Count of Exchange batteries are:"+counts.exchange+"\n");
+    System.out.println("Count of Failed batteries are:"+counts.failed+"\n");
     System.out.println("Done counting :)\n");
   }
 
