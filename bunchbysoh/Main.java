82b6d20d155809407641724ab2f8e2d2941c32e0
M Shashank Dev Padakannaya <99595131+Shasdev@users.noreply.github.com>
2023-12-03 17:45:22 +0530

    Update Main.java

--- bunchbysoh/Main.java
+++ bunchbysoh/Main.java
@@ -1,6 +1,7 @@
 package bunchbysoh;
 
 public class Main {
+ 
   static class CountsBySoH {
     public int healthy = 0;
     public int exchange = 0;
@@ -35,13 +36,13 @@ public class Main {
     assert(counts.healthy == 2);
     assert(counts.exchange == 3);
     assert(counts.failed == 1);
-    System.out.println("Count of Healthy batteries are:"+counts.healthy+"\n");
-    System.out.println("Count of Exchange batteries are:"+counts.exchange+"\n");
-    System.out.println("Count of Failed batteries are:"+counts.failed+"\n");
+    System.out.println("Count of healthy batteries : "+counts.healthy+"\n");
+    System.out.println("Count of exchange batteries : "+counts.exchange+"\n");
+    System.out.println("Count of failed batteries : "+counts.failed+"\n");
     System.out.println("Done counting :)\n");
   }
-
-  public static void main(String[] args) {
+   public static void main(String[] args) {
     testBucketingByHealth();
   }
+  
 }
