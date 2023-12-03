3559f8d6e7fbc30c2ba27c0a4a1b72dd541d766a
M Shashank Dev Padakannaya <99595131+Shasdev@users.noreply.github.com>
2023-12-03 17:52:34 +0530

    Update Main.java

--- bunchbysoh/Main.java
+++ bunchbysoh/Main.java
@@ -36,6 +36,33 @@ public class Main {
     assert(counts.healthy == 2);
     assert(counts.exchange == 3);
     assert(counts.failed == 1);
+
+    int[] emptyArray = {};
+    CountsBySoH emptyArrayCounts = countBatteriesByHealth(emptyArray);
+    assert(emptyArrayCounts.healthy == 0);
+    assert(emptyArrayCounts.exchange == 0);
+    assert(emptyArrayCounts.failed == 0);
+
+    int[] fullCapacity = {120, 120, 120};
+    CountsBySoH fullCapacityCounts = countBatteriesByHealth(fullCapacity);
+    assert(fullCapacityCounts.healthy == 3);
+    assert(fullCapacityCounts.exchange == 0);
+    assert(fullCapacityCounts.failed == 0);
+
+    int[] minCapacity = {0, 10, 20};
+    CountsBySoH minCapacityCounts = countBatteriesByHealth(minCapacity);
+    assert(minCapacityCounts.healthy == 0);
+    assert(minCapacityCounts.exchange == 0);
+    assert(minCapacityCounts.failed == 3);
+
+    int[] maxCapacity = {120, 119, 120};
+    CountsBySoH maxCapacityCounts = countBatteriesByHealth(maxCapacity);
+    assert(maxCapacityCounts.healthy == 2);
+    assert(maxCapacityCounts.exchange == 1);
+    assert(maxCapacityCounts.failed == 0);
+
+    
+   
     System.out.println("Count of healthy batteries : "+counts.healthy+"\n");
     System.out.println("Count of exchange batteries : "+counts.exchange+"\n");
     System.out.println("Count of failed batteries : "+counts.failed+"\n");
